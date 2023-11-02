9bc92db310204e11d4d0be558525cd4664127145
MrNightFury <73826973+MrNightFury@users.noreply.github.com>
2023-11-02 15:41:08 +0300

    Update subscribe.ts

--- src/subscribe.ts
+++ src/subscribe.ts
@@ -4,7 +4,7 @@ import fs from "fs";
 import { Topic } from "./Topic";
 import { Config } from "./Config";
 
-export async function subscribe(cfg: any, topics: Topic[], callback: any) {
+export async function subscribe(cfg: any, topics: Topic[], callback: any) {    
     let client = await mqtt.connectAsync({
         host: cfg.host,
         protocol: "mqtt"
@@ -17,21 +17,19 @@ export async function subscribe(cfg: any, topics: Topic[], callback: any) {
         return;
     }
 
-    client.on('connect', () => {
-        console.log("Connected to chemodan");
-        if(!client) {
-            return;
-        }
-        for (let topic of topics) {
-            client.subscribe(topic.getPath(), err => {
-                if (!err) {
-                    console.log("Subscribed to " + topic.getPath());
-                } else {
-                    console.log(err);
-                }
-            })
-        }
-    });
+    console.log("Connected to chemodan");
+    if(!client) {
+        return;
+    }
+    for (let topic of topics) {
+        client.subscribe(topic.getPath(), err => {
+            if (!err) {
+                console.log("Subscribed to " + topic.getPath());
+            } else {
+                console.log(err);
+            }
+        })
+    }
 
     client.on('message', (topic, message) => {
         callback(topic, message);
