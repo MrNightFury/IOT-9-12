bbe923a821c48f940740578b0595eaca1b3cc09c
MrNightFury <73826973+MrNightFury@users.noreply.github.com>
2023-11-30 15:38:46 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -89,13 +89,13 @@ let rpc: RPCTopic[] = [
                 let mes = "1";
                 let top = new Topic("buzzer", "enabled").getPath() + "/on";
                 connection.publish(top, mes);
-                rpc(JSON.stringify({"actual_isBuzzerOn": true}))
+                rpc(JSON.stringify({"actual_isBuzzerOn": "true"}))
             }
             if (message.method == "turnBuzzerOff") {
                 let mes = "0";
                 let top = new Topic("buzzer", "enabled").getPath() + "/on";
                 connection.publish(top, mes);
-                rpc(JSON.stringify({"actual_isBuzzerOn": false}))
+                rpc(JSON.stringify({"actual_isBuzzerOn": "false"}))
             }
             if (message.method == "setFrequency") {
                 let mes = "" + message.params.frequency;
